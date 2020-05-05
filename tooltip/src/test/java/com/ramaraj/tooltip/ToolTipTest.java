909bde2c825ad623ba87786a85af2eecfe3277a0
Ramaraj Thangapandi <jsramraj@gmail.com>
2020-05-05 11:57:55 +0530

    add unit test to the tooltip and tooltip builder classes

---
+++ tooltip/src/test/java/com/ramaraj/tooltip/ToolTipTest.java
@@ -0,0 +1,16 @@
+package com.ramaraj.tooltip;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ToolTipTest {
+    @Test
+    public void verifyStaticToolTipCreation() {
+        StaticTip staticTip = new StaticTip("MainActivity", 123, "Tip title", "Tip message");
+
+        assertEquals("MainActivity", staticTip.getActivityName());
+        assertEquals(123, staticTip.getResourceId());
+        assertEquals("Tip message", staticTip.getTipText());
+    }
+}
